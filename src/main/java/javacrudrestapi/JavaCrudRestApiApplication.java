5fc54d8f8c595520b756c3c0d42f0d24d1d8e78e
Alexis <alexis.castellano@gmail.com>
2024-04-13 07:53:24 -0300

    init commit

---
+++ src/main/java/javacrudrestapi/JavaCrudRestApiApplication.java
@@ -0,0 +1,13 @@
+package javacrudrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCrudRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/javacrudrestapi/JavaCrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package javacrudrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
